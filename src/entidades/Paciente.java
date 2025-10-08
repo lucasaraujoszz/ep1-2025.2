2b102f3396ccde2517414f2b1e694bb3db9fd6ea
Lucas Araújo Lima <lucasaraujolima929@gmail.com>
2025-10-08 00:58:58 -0300

    feat: Adiciona métodos toCSV e fromCSV em Paciente

--- src/entidades/Paciente.java
+++ src/entidades/Paciente.java
@@ -1,13 +1,24 @@
-package entities;
+package entidades;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Paciente extends Pessoa {
-    private int idade;
 
+    private int idade;
+    private List<Consulta> historicoConsultas;
+    private List<Internacao> historicoInternacoes;
+
+    // Construtor
     public Paciente(String nome, String cpf, int idade) {
-        super(nome, cpf); // chama o construtor da classe Pessoa
+        super(nome, cpf);
         this.idade = idade;
+        // Inicializa as listas para evitar erros
+        this.historicoConsultas = new ArrayList<>();
+        this.historicoInternacoes = new ArrayList<>();
     }
 
+    // Getters e Setters...
     public int getIdade() {
         return idade;
     }
@@ -16,8 +27,36 @@ public class Paciente extends Pessoa {
         this.idade = idade;
     }
 
+    // Métodos para gerenciar o histórico
+    public List<Consulta> getHistoricoConsultas() {
+        return historicoConsultas;
+    }
+
+    public void adicionarConsultaAoHistorico(Consulta consulta) {
+        this.historicoConsultas.add(consulta);
+    }
+
+     public List<Internacao> getHistoricoInternacoes() {
+        return historicoInternacoes;
+    }
+
+    public void adicionarInternacaoAoHistorico(Internacao internacao) {
+        this.historicoInternacoes.add(internacao);
+    }
+
+
     @Override
     public String toString() {
-        return "Paciente [" + super.toString() + ", Idade: " + idade + "]";
+        return "Paciente [" + super.toString() + ", Idade=" + idade + "]";
+    }
+    
+    // Formato CSV: cpf;nome;idade
+    public String toCSV() {
+        return getCpf() + ";" + getNome() + ";" + getIdade();
+    }
+
+    public static Paciente fromCSV(String csvLine) {
+        String[] fields = csvLine.split(";");
+        return new Paciente(fields[1], fields[0], Integer.parseInt(fields[2]));
     }
 }
